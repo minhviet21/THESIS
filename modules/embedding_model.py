ec992b64dff6cd96543fb4532c7c8abff341412e
minhviet1 <viet.doan@boot.ai>
2025-03-14 14:47:52 +0700

    small update

--- modules/embedding_model.py
+++ modules/embedding_model.py
@@ -3,7 +3,7 @@ from pyvi.ViTokenizer import tokenize
 from sentence_transformers import SentenceTransformer
 
 class EmbeddingModel():
-    def __init__(self, name, is_tokenized = False):
+    def __init__(self, name, is_tokenized = False, is_instruted = False):
         device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = SentenceTransformer(name).to(device)
         self.is_tokenized = is_tokenized
@@ -11,4 +11,7 @@ class EmbeddingModel():
     def encode(self, texts):
         if self.is_tokenized:
             texts = [tokenize(text) for text in texts]
+        if self.is_instruted:
+            task_description = "Given a query, retrieve the most relevant documents that can answer the query"
+            texts = [f'Instruct: {task_description}\nQuery: {text}' for text in texts]
         return self.model.encode(texts)
