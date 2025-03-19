a9d693f4ce70bebcc08acd58a3d9ae26b1d5b0c1
minhviet1 <viet.doan@boot.ai>
2025-03-19 10:37:40 +0700

    small update

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import ast
+from sklearn.model_selection import train_test_split
 
 raw_documents = pd.read_json("data/raw/demo.json")
-raw_test = pd.read_csv("data/raw/test_v4.csv")
-raw_test = raw_test[raw_test["positives"] != "[]"]
-raw_test = raw_test.reset_index(drop=True)
+raw_data = pd.read_csv("data/raw/test_v4.csv")
+raw_data = raw_data[raw_data["positives"] != "[]"]
+raw_data = raw_data.reset_index(drop=True)
 
 
 raw_documents['index'] = raw_documents.index
@@ -16,6 +17,9 @@ def create_index(positives):
     positive_indexs = [int(raw_documents[raw_documents["context"] == positive]["index"].values[0]) for positive in positives] 
     return positive_indexs
 
-raw_test["positive_indexs"] = raw_test["positives"].apply(create_index)
-raw_test["index"] = raw_test.index
-raw_test.to_csv("data/clean/test.csv", index=False)
+raw_data["positive_indexs"] = raw_data["positives"].apply(create_index)
+raw_data["index"] = raw_data.index
+train_data, test_data = train_test_split(raw_data, test_size=0.5, random_state=42)
+
+train_data.to_csv("data/clean/train_data.csv", index=False)
+test_data.to_csv("data/clean/test_data.csv", index=False)
