8161fb7b076867015f1e3f05229f232680e4b05e
minhviet1 <viet.doan@boot.ai>
2025-03-31 00:22:27 +0700

    small update

--- evaluate.py
+++ evaluate.py
@@ -1,60 +1,60 @@
-import pandas as pd
-import ast
-from modules.database import Database
-from modules.model import BiEncoder, CrossEncoder
+# import pandas as pd
+# import ast
+# from modules.database import Database
+# from modules.model import BiEncoder, CrossEncoder
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
-WEAVIATE_URL = os.getenv("WEAVIATE_URL")
-WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+# WEAVIATE_URL = os.getenv("WEAVIATE_URL")
+# WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
 
-collection_name = ""
-evaluate_data_path = ""
-bi_encoder_path = ""
-cross_encoder_path = ""
+# collection_name = ""
+# evaluate_data_path = ""
+# bi_encoder_path = ""
+# cross_encoder_path = ""
 
 
-database = Database(url = WEAVIATE_URL, api_key = WEAVIATE_API_KEY)
-evaluate_data = pd.read_csv(evaluate_data_path)
-biencoder = BiEncoder(bi_encoder_path)
-crossencoder = CrossEncoder(cross_encoder_path)
+# database = Database(url = WEAVIATE_URL, api_key = WEAVIATE_API_KEY)
+# evaluate_data = pd.read_csv(evaluate_data_path)
+# biencoder = BiEncoder(bi_encoder_path)
+# crossencoder = CrossEncoder(cross_encoder_path)
 
-def evaluate(grouth_truths, predictions):
-    true_count = 0
-    for grouth_truth, prediction in zip(grouth_truths, predictions):
-        if bool(set(grouth_truth) & set(prediction)):
-            true += 1
-    return true, len(grouth_truths) - true_count
+# def evaluate(grouth_truths, predictions):
+#     true_count = 0
+#     for grouth_truth, prediction in zip(grouth_truths, predictions):
+#         if bool(set(grouth_truth) & set(prediction)):
+#             true += 1
+#     return true, len(grouth_truths) - true_count
 
-def search_pipeline(hybrid_alpha, top_search, top_rerank, questions, is_rerank):
-    vectors = biencoder.encode(questions)
-    search_results = [database.search(collection_name, question, vector, hybrid_alpha, top_search) 
-                        for (question, vector) in zip(questions, vectors)]
+# def search_pipeline(hybrid_alpha, top_search, top_rerank, questions, is_rerank):
+#     vectors = biencoder.encode(questions)
+#     search_results = [database.search(collection_name, question, vector, hybrid_alpha, top_search) 
+#                         for (question, vector) in zip(questions, vectors)]
     
-    if is_rerank:
-        rerank_results = [crossencoder.rerank(question, search_result["context"], search_result["index"], top_rerank) 
-                        for question, search_result in zip(questions, search_results)]
-        return [rerank_result["index"] for rerank_result in rerank_results]
-    else:
-        return [search_result["index"] for search_result in search_results]
+#     if is_rerank:
+#         rerank_results = [crossencoder.rerank(question, search_result["context"], search_result["index"], top_rerank) 
+#                         for question, search_result in zip(questions, search_results)]
+#         return [rerank_result["index"] for rerank_result in rerank_results]
+#     else:
+#         return [search_result["index"] for search_result in search_results]
 
 
-true, false = 0, 0
-batch_size = 50
-hybrid_alpha = 0.5
-top_search = 10
-top_rerank = 5
+# true, false = 0, 0
+# batch_size = 50
+# hybrid_alpha = 0.5
+# top_search = 10
+# top_rerank = 5
 
-for i in range(0, len(evaluate_data), batch_size):
-    batch = evaluate_data.iloc[i:i + batch_size] 
-    questions = batch["question"].tolist()
-    positive_indexs = batch["positive_indexs"].tolist()
-    grouth_truths = [ast.literal_eval(idx) for idx in positive_indexs]
+# for i in range(0, len(evaluate_data), batch_size):
+#     batch = evaluate_data.iloc[i:i + batch_size] 
+#     questions = batch["question"].tolist()
+#     positive_indexs = batch["positive_indexs"].tolist()
+#     grouth_truths = [ast.literal_eval(idx) for idx in positive_indexs]
 
-    search_results = search_pipeline(hybrid_alpha, top_search, top_rerank, questions, False)
-    true_, false_ = evaluate(search_results, grouth_truths)
-    true += true_
-    false += false_
+#     search_results = search_pipeline(hybrid_alpha, top_search, top_rerank, questions, False)
+#     true_, false_ = evaluate(search_results, grouth_truths)
+#     true += true_
+#     false += false_
 
-    print(f"True: {true}, False: {false}")
+#     print(f"True: {true}, False: {false}")
--- modules/model.py
+++ modules/model.py
@@ -2,7 +2,7 @@ import torch
 from pyvi.ViTokenizer import tokenize
 from sentence_transformers import SentenceTransformer
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
-import py_vncorenlp
+# import py_vncorenlp
 
 class BiEncoder():
     def __init__(self, name, is_tokenized = False, is_instructed = False):
@@ -19,36 +19,36 @@ class BiEncoder():
             texts = [f'Instruct: {task_description}\nquestion: {text}' for text in texts]
         return self.model.encode(texts)
 
-class CrossEncoder():
-    def __init__(self, name, is_tokenized = False, pre_tokenize_path = "/kaggle/working/"):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.name = name
-        self.tokenizer = AutoTokenizer.from_pretrained(self.name)
-        self.model = AutoModelForSequenceClassification.from_pretrained(self.name).to(self.device).eval()
-        self.is_tokenized = is_tokenized
-        if is_tokenized:
-            py_vncorenlp.download_model(save_dir=pre_tokenize_path)
-            self.pre_tokenizer = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=pre_tokenize_path)
+# class CrossEncoder():
+#     def __init__(self, name, is_tokenized = False, pre_tokenize_path = "/kaggle/working/"):
+#         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#         self.name = name
+#         self.tokenizer = AutoTokenizer.from_pretrained(self.name)
+#         self.model = AutoModelForSequenceClassification.from_pretrained(self.name).to(self.device).eval()
+#         self.is_tokenized = is_tokenized
+#         if is_tokenized:
+#             py_vncorenlp.download_model(save_dir=pre_tokenize_path)
+#             self.pre_tokenizer = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=pre_tokenize_path)
 
-    def compute_score(self, pairs):
-        if self.name.startswith("'itdainb'"):
-            pairs = [[self.pre_tokenizer.word_segment(sent) for sent in sents] for sents in pairs]
-            with torch.no_grad():
-                inputs = self.tokenizer(pairs, padding=True, truncation="longest_first", return_tensors='pt', max_length=256).to(self.device)
-                predictions = self.model(**inputs, return_dict=True).logits
-                logits = torch.nn.Sigmoid()(predictions)
-                scores = [logit[0] for logit in logits]
+#     def compute_score(self, pairs):
+#         if self.name.startswith("'itdainb'"):
+#             pairs = [[self.pre_tokenizer.word_segment(sent) for sent in sents] for sents in pairs]
+#             with torch.no_grad():
+#                 inputs = self.tokenizer(pairs, padding=True, truncation="longest_first", return_tensors='pt', max_length=256).to(self.device)
+#                 predictions = self.model(**inputs, return_dict=True).logits
+#                 logits = torch.nn.Sigmoid()(predictions)
+#                 scores = [logit[0] for logit in logits]
 
-        elif self.name.startswith("BAAI"):
-            with torch.no_grad():
-                inputs = self.tokenizer(pairs, padding=True, truncation=True, return_tensors='pt', max_length=512).to(self.device)
-                predictions = self.model(**inputs, return_dict=True).logits.view(-1, ).float()
+#         elif self.name.startswith("BAAI"):
+#             with torch.no_grad():
+#                 inputs = self.tokenizer(pairs, padding=True, truncation=True, return_tensors='pt', max_length=512).to(self.device)
+#                 predictions = self.model(**inputs, return_dict=True).logits.view(-1, ).float()
 
-        return scores
+#         return scores
     
-    def rerank(self, question, contexts, indexs, top_k):
-        pairs = [[question, context] for context in contexts]
-        scores = self.compute_score(pairs)
-        result = [{"index": index, "context": context, "score": score} for index, context, score in zip(indexs, contexts, scores)]
-        result = sorted(result, key=lambda x: x["score"], reverse=True)[:top_k]
-        return result
+#     def rerank(self, question, contexts, indexs, top_k):
+#         pairs = [[question, context] for context in contexts]
+#         scores = self.compute_score(pairs)
+#         result = [{"index": index, "context": context, "score": score} for index, context, score in zip(indexs, contexts, scores)]
+#         result = sorted(result, key=lambda x: x["score"], reverse=True)[:top_k]
+#         return result
