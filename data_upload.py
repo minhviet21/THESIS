eb72bc32184ebc32a4d9360a314ff449724a077b
minhviet1 <viet.doan@boot.ai>
2025-03-31 00:13:06 +0700

    small update

--- data_upload.py
+++ data_upload.py
@@ -1,26 +1,26 @@
 import pandas as pd
 from modules.database import Database
 from modules.model import BiEncoder
+
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 WEAVIATE_URL = os.getenv("WEAVIATE_URL")
 WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
 
-collection_name = ""
-bi_encoder_path = ""
-document_path = ""
+collection_name = "vietnamese_bi_encoder_1"
+bi_encoder_path = "models/vietnamese-bi-encoder"
+document_path = "data/clean/documents.csv"
 
 database = Database(url = WEAVIATE_URL, api_key = WEAVIATE_API_KEY)
 database.create_collection(collection_name)
 
 model = BiEncoder(bi_encoder_path)
 
-data = pd.read_csv(document_path).head(25)
+data = pd.read_csv(document_path)
 documents = data["context"].tolist()
 
-batch_size = 5
+batch_size = 50
 for i in range(0, len(documents), batch_size):
     indexs = list(range(i, i+batch_size))
     texts = documents[i:i+batch_size]
