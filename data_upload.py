d62852b3fc43d78f0e9f301c1bd71b16d413a4d7
minhviet1 <viet.doan@boot.ai>
2025-03-31 14:17:27 +0700

    small update

--- data_upload.py
+++ data_upload.py
@@ -1,32 +1,32 @@
-# import pandas as pd
-# from modules.database import Database
-# from modules.model import BiEncoder
+import pandas as pd
+from modules.database import Database
+from modules.model import BiEncoder
 
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-# WEAVIATE_URL = os.getenv("WEAVIATE_URL")
-# WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
+import os
+from dotenv import load_dotenv
+load_dotenv()
+WEAVIATE_URL = os.getenv("WEAVIATE_URL")
+WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
 
-# collection_name = "vietnamese_bi_encoder_1"
-# bi_encoder_path = "models/vietnamese-bi-encoder"
-# document_path = "data/clean/documents.csv"
+collection_name = "vietnamese_bi_encoder_1"
+bi_encoder_path = "models/vietnamese-bi-encoder"
+document_path = "data/clean/documents.csv"
 
-# database = Database(url = WEAVIATE_URL, api_key = WEAVIATE_API_KEY)
-# database.create_collection(collection_name)
+database = Database(url = WEAVIATE_URL, api_key = WEAVIATE_API_KEY)
+database.create_collection(collection_name)
 
-# model = BiEncoder(bi_encoder_path)
+model = BiEncoder(bi_encoder_path)
 
-# data = pd.read_csv(document_path)
-# documents = data["context"].tolist()
+data = pd.read_csv(document_path)
+documents = data["context"].tolist()
 
-# batch_size = 50
-# for i in range(0, len(documents), batch_size):
-#     indexs = list(range(i, i+batch_size))
-#     texts = documents[i:i+batch_size]
-#     vectors = model.encode(texts)
-#     database.upload_data(collection_name, indexs, texts, vectors)
-#     print(f"Uploaded {i+len(texts)}/{len(documents)}")
+batch_size = 50
+for i in range(0, len(documents), batch_size):
+    indexs = list(range(i, i+batch_size))
+    texts = documents[i:i+batch_size]
+    vectors = model.encode(texts)
+    database.upload_data(collection_name, indexs, texts, vectors)
+    print(f"Uploaded {i+len(texts)}/{len(documents)}")
 
-# database.close()
-# print("Done")
+database.close()
+print("Done")
